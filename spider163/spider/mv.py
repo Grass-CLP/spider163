40ae48865bb0825dcc28393de4188be34ddd7c85
Lipson <chenlipeng@cmschina.com.cn>
2018-04-17 16:30:47 +0800

    + mv base func

--- spider163/spider/mv.py
+++ spider163/spider/mv.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from terminaltables import AsciiTable
 
 from spider163 import settings
 from spider163.spider import public as uapi
@@ -10,8 +9,11 @@ from spider163.spider.common import encSecKey, create_params_by_dict
 from spider163.utils import pylog
 from spider163.utils import tools
 
-default_r = 720
-max_r = default_r
+r_1080p = 1808
+r_720p = 720
+r_480p = 480
+r_240p = 240
+default_r = r_720p
 
 
 class MV:
@@ -20,41 +22,6 @@ class MV:
         self.session = settings.Session()
         self.__encSecKey = encSecKey
         self.default_r = 720
-
-    def view_down_(self, playlist_id, path="."):
-        list = self.get_playlist(str(playlist_id))
-        msg = {"success": 0, "failed": 0, "failed_list": []}
-        for music in list['tracks']:
-            pylog.print_info(
-                "正在下载歌曲 {}-{}.mp3".format(
-                    tools.encode(music['name']),
-                    tools.encode(music['artists'][0]['name'])
-                )
-            )
-            link = self.get_mv_link(music["id"])
-            if link is None:
-                msg["failed"] = msg["failed"] + 1
-                msg["failed_list"].append(music)
-                continue
-            r = requests.get(link)
-            with open("{}/{}-{}{}".format(
-                    path,
-                    tools.encode(music['name']).replace("/", "-"),
-                    tools.encode(music['artists'][0]['name']).replace("/", "-"),
-                    ".mp3"
-            ), "wb") as code:
-                code.write(r.content)
-                msg["success"] = msg["success"] + 1
-        pylog.print_warn(
-            "下载成功：{} 首，下载失败：{}首".format(msg["success"], msg["failed"])
-        )
-        tb = [["歌曲名字", "艺术家", "ID"]]
-        for music in msg["failed_list"]:
-            n = music['name'].encode("utf-8")
-            a = music['artists'][0]['name'].encode("utf-8")
-            i = music['id']
-            tb.append([n, a, i])
-        print(AsciiTable(tb).table)
 
     def get_playlist(self, playlist_id):
         url = uapi.playlist_api.format(playlist_id)
@@ -65,48 +32,34 @@ class MV:
         except Exception as e:
             raise
 
-    def get_target_r(self, obj, limit_r=max_r):
-        pass
+    def get_target_r(self, obj, limit_r=default_r):
+        max_valid = max([x['br'] for x in obj['brs']])
+        return limit_r if max_valid > limit_r else max_valid
 
-    def view_down(self, mv_id, path=".", r = max_r):
+    def view_down(self, mv_id, path=".", r=default_r):
         detail = self.get_mv_detail(mv_id=mv_id)
-
-        list = self.get_playlist(str(playlist_id))
-        msg = {"success": 0, "failed": 0, "failed_list": []}
-        for music in list['tracks']:
-            pylog.print_info(
-                "正在下载歌曲 {}-{}.mp3".format(
-                    tools.encode(music['name']),
-                    tools.encode(music['artists'][0]['name'])
-                )
+        target_r = self.get_target_r(detail, r)
+        link = self.get_mv_link(mv_id, target_r)
+        pylog.print_info(
+            "正在下载MV {}-{}.mp4".format(
+                tools.encode(detail['name']),
+                tools.encode(detail['artistName'])
             )
-            link = self.get_mv_link(music["id"])
-            if link is None:
-                msg["failed"] = msg["failed"] + 1
-                msg["failed_list"].append(music)
-                continue
-            r = requests.get(link)
-            with open("{}/{}-{}{}".format(
-                    path,
-                    tools.encode(music['name']).replace("/", "-"),
-                    tools.encode(music['artists'][0]['name']).replace("/", "-"),
-                    ".mp3"
-            ), "wb") as code:
-                code.write(r.content)
-                msg["success"] = msg["success"] + 1
-        pylog.print_warn(
-            "下载成功：{} 首，下载失败：{}首".format(msg["success"], msg["failed"])
         )
-        tb = [["歌曲名字", "艺术家", "ID"]]
-        for music in msg["failed_list"]:
-            n = music['name'].encode("utf-8")
-            a = music['artists'][0]['name'].encode("utf-8")
-            i = music['id']
-            tb.append([n, a, i])
-        print(AsciiTable(tb).table)
+
+        req = requests.get(link)
+        with open("{}/{}-{}{}".format(
+                path,
+                tools.encode(detail['artistName']).replace("/", "-"),
+                tools.encode(detail['name']).replace("/", "-"),
+                ".mp4"
+        ).decode('utf-8'), "wb") as code:
+            code.write(req.content)
+
+        pylog.print_warn("下载成功")
 
     def get_mv_detail(self, mv_id):
-        obj = {'id': mv_id, 'csrf_token':'csrf'}
+        obj = {'id': mv_id, 'csrf_token': 'csrf'}
         data = {
             'params': create_params_by_dict(obj),
             'encSecKey': self.__encSecKey
@@ -121,8 +74,8 @@ class MV:
         except Exception as e:
             raise
 
-    def get_mv_link(self, mv_id, r=default_r):
-        obj = {'id': str(mv_id), 'r': str(r), 'csrf_token': 'csrf'}
+    def get_mv_link(self, mv_id, r):
+        obj = {'id': mv_id, 'r': r, 'csrf_token': 'csrf'}
         data = {
             'params': create_params_by_dict(obj),
             'encSecKey': self.__encSecKey
--- spider163/test/mv.py
+++ spider163/test/mv.py
@@ -4,5 +4,14 @@
 # created by Lipson on 2018/4/16.
 # email to LipsonChan@yahoo.com
 #
+import json
 
+from spider163.spider.mv import MV, r_240p
 
+mv = MV()
+link = mv.get_mv_link(5324139, 720)
+detail = mv.get_mv_detail(5324139)
+mv.view_down(5324139, r=r_240p)
+
+print(link)
+print(json.dumps(detail))
